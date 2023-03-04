48f7084c4f61352c995444a80f2fecea8e4aefde
Franco Celiz <franco7472@hotmail.com>
2023-03-04 05:26:15 -0300

    Challenge-reactJS

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import './App.css';
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+function App() {
+  const [cat, setCat] = useState();
+
+  const handleSubmit = (e) =>{
+    axios.get("https://some-random-api.ml/animal/cat")//uso axios para llamar a la API y luego seteo la respuesta
+    .then((res) =>{
+    setCat(res);
+    });
+  }
+
+  useEffect(() =>{
+    handleSubmit()
+  }, []);
+
+  return (
+    <section className="main-container">
+      <div className="div-container">
+        <span>{cat.data.fact} </span>
+        <img className="img-animal" src={cat.data.image} alt="gato" />
+        <button onClick={handleSubmit}>Next fact</button>
+      </div>
+    </section>
+  );
+}
+
+export default App;
